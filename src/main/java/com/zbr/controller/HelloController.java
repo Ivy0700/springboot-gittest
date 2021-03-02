e1d5c7f39a6b1ed734b43fa449ef16ec59ca4098
Ivy_glb <Ivy_glb@gmail.com>
2021-03-02 15:04:32 +0800

    first commit

--- src/main/java/com/zbr/controller/HelloController.java
+++ src/main/java/com/zbr/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("info")
     public String info(){
-        return "Hello SpringBoot Info:";
+        return "Hello SpringBoot Info:dd";
     }
 }
